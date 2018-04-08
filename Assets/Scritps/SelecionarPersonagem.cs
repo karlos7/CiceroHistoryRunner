aa764de6e4b9c2f83190cf65de6d6720c4f907c7
Carlos Williamy <carloswilliamylourenco@gmail.com>
2018-04-08 20:57:18 -0300

    Selecionar personagem

--- Assets/Scritps/SelecionarPersonagem.cs
+++ Assets/Scritps/SelecionarPersonagem.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SelecionarPersonagem : MonoBehaviour {
     int sexo = 1;
@@ -11,10 +12,15 @@ public class SelecionarPersonagem : MonoBehaviour {
     public GameObject sel2;
     public GameObject sel3;
     public GameObject sel4;
+    public Image imagem;
+    public Sprite img1;
+    public Sprite img2;
+    public Sprite img3;
+    public Sprite img4;
     // Use this for initialization
     void Start () {
-		
-	}
+        
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -40,23 +46,27 @@ public class SelecionarPersonagem : MonoBehaviour {
     public void masculino()
     {
         sexo = 1;
+        if (cor == 1) { imagem.sprite = img1; } else { imagem.sprite = img2; }
         sel1.SetActive(true);
         sel3.SetActive(false);
     }
     public void feminino()
     {
+        if (cor == 1) { imagem.sprite = img3; } else { imagem.sprite = img4; }
         sexo = 2;
         sel1.SetActive(false);
         sel3.SetActive(true);
     }
     public void cor1()
     {
+        if (sexo == 1) { imagem.sprite = img1; } else { imagem.sprite = img3; }
         cor = 1;
         sel2.SetActive(true);
         sel4.SetActive(false);
     }
     public void cor2()
     {
+        if (sexo == 1) { imagem.sprite = img2; } else { imagem.sprite = img4; }
         cor = 2;
         sel2.SetActive(false);
         sel4.SetActive(true);
--- Assets/Scritps/spawnPlayer.cs
+++ Assets/Scritps/spawnPlayer.cs
@@ -7,6 +7,8 @@ public class spawnPlayer : MonoBehaviour {
     public GameObject Player;
     private GameObject PlayerJogo;
     public GameObject Player2;
+    public GameObject Player3;
+    public GameObject Player4;
 
     // Use this for initialization
     void Start () {
@@ -18,6 +20,16 @@ public class spawnPlayer : MonoBehaviour {
         else if (PlayerPrefs.GetInt("personagem") == 2)
         {
             PlayerJogo = Instantiate(Player2) as GameObject;
+            PlayerJogo.transform.position = new Vector3(-42.34f, 9f, -3f);
+        }
+        else if (PlayerPrefs.GetInt("personagem") == 3)
+        {
+            PlayerJogo = Instantiate(Player3) as GameObject;
+            PlayerJogo.transform.position = new Vector3(-42.34f, 9f, -3f);
+        }
+        else
+        {
+            PlayerJogo = Instantiate(Player4) as GameObject;
             PlayerJogo.transform.position = new Vector3(-42.34f, 9f, -3f);
         }
     }
