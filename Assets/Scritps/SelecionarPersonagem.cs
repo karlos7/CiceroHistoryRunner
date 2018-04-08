b8617fa917b525ab60d23e3e7c72c18473cea994
Carlos Williamy <carloswilliamylourenco@gmail.com>
2018-04-08 15:50:58 -0300

    telaPersonagem

--- Assets/Scritps/SelecionarPersonagem.cs
+++ Assets/Scritps/SelecionarPersonagem.cs
@@ -7,8 +7,12 @@ public class SelecionarPersonagem : MonoBehaviour {
     int sexo = 1;
     int cor = 1;
     int personagem;
-	// Use this for initialization
-	void Start () {
+    public GameObject sel1;
+    public GameObject sel2;
+    public GameObject sel3;
+    public GameObject sel4;
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
@@ -36,18 +40,26 @@ public class SelecionarPersonagem : MonoBehaviour {
     public void masculino()
     {
         sexo = 1;
+        sel1.SetActive(true);
+        sel3.SetActive(false);
     }
     public void feminino()
     {
         sexo = 2;
+        sel1.SetActive(false);
+        sel3.SetActive(true);
     }
     public void cor1()
     {
         cor = 1;
+        sel2.SetActive(true);
+        sel4.SetActive(false);
     }
     public void cor2()
     {
         cor = 2;
+        sel2.SetActive(false);
+        sel4.SetActive(true);
     }
     public void continuar()
     {
