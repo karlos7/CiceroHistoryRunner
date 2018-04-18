5f0b7d89b6b3159660ba991f8ce25c96ed0701ec
Carlos Williamy <carloswilliamylourenco@gmail.com>
2018-04-17 22:07:08 -0300

    game over

---
+++ Assets/Scritps/Chapeu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chapeu : MonoBehaviour {
+    private float currentTime;
+    public GameObject chapeu;
+    private float x;
+    // Use this for initialization
+    void Start () {
+        currentTime = 0;
+        x = 0.15f;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime += Time.deltaTime;
+        if (currentTime >= 3)
+        {
+            Destroy(chapeu);
+        }
+        else
+        {
+            x +=1f;
+            transform.position = new Vector3(x, transform.position.y, 50f);
+        }
+    }
+}
